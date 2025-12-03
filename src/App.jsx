3032f88639d109bc27e07d6da7c0e0c992c1ecfa
Richard Le <169632088+rlechuong@users.noreply.github.com>
2025-12-03 18:32:21 -0500

    Integrate ESPN API and test fetching data

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [teams, setTeams] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://site.api.espn.com/apis/site/v2/sports/basketball/nba/teams")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Full API Response:", data);
+        console.log("Teams Array:", data.sports[0].leagues[0].teams);
+        console.log("First Team Details:", data.sports[0].leagues[0].teams[0].team);
+        setTeams(data.sports[0].leagues[0].teams);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error Fetching Teams:", error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <div>Loading</div>;
+  }
+
   return (
     <div className="App">
-      <h1>Memory Card Game</h1>
+      <h1>Memory Card</h1>
+      <p>API Data in the browser console!</p>
+      <p>Number Of Teams: {teams.length}</p>
     </div>
   );
 }
